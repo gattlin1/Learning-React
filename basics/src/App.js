c9efcbced004dd1da05a9491ffb855c16fd019c9
gattlin1 <gattlin1@live.missouristate.edu>
2020-12-13 19:18:38 -0500

    Adding two way binding

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props =>  {
-  const [personState, setPersonState] = useState({
+class App extends Component  {
+  state = {
       persons: [
         { name: 'Max', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Steve', age: 27 }
       ],
-  });
-  const [otherState, ] = useState('some other state value');
+  }
 
-  console.log(personState, otherState);
+  otherState = 'some other state value'
 
-  const switchNameHandler = (newName) => {
-    setPersonState({
+  switchNameHandler = (newName) => {
+    this.setState({
       persons: [
         { name: newName, age: 23 },
         { name: 'Manu', age: 29 },
@@ -23,26 +22,39 @@ const App = props =>  {
       ],
     })
   }
-  return (
-    <div className="App">
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 23 },
+        { name: event.target.value, age: 29 },
+        { name: 'Steve', age: 27 }
+      ],
+    })
+  }
+  render () {
+    return (
+      <div className="App">
       <h1>Hi, I'm a react app</h1>
-      <button onClick={ () => switchNameHandler('Gattlin') }>Switch Name</button>
+      <button onClick={ () => this.switchNameHandler('Gattlin') }>Switch Name</button>
       <Person
-        name={ personState.persons[0].name }
-        age={ personState.persons[0].age }
-      />
+        name={ this.state.persons[0].name }
+        age={ this.state.persons[0].age }
+        />
       <Person
-        name={ personState.persons[1].name }
-        age={ personState.persons[1].age }
-        click={ switchNameHandler.bind(this, 'Jim') } >
+        name={ this.state.persons[1].name }
+        age={ this.state.persons[1].age }
+        click={ this.switchNameHandler.bind(this, 'Jim') }
+        changed= {this.nameChangeHandler}>
         My Hobbies: Racing
       </Person>
       <Person
-        name={ personState.persons[2].name }
-        age={ personState.persons[2].age }
-      />
+        name={ this.state.persons[2].name }
+        age={ this.state.persons[2].age }
+        />
     </div>
-  );
+    );
+  }
 
     // This is equivalent to above
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, "Hi, I'm a React App"));
--- basics/src/Person/Person.js
+++ basics/src/Person/Person.js
@@ -7,6 +7,7 @@ const Person = (props) => {
                 { props.name } is { props.age } years old
             </p>
             <p>{ props.children }</p>
+            <input type="text" onChange={ props.changed } value={ props.name }/>
         </div>
     )
 }
