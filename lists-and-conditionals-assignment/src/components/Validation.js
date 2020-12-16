f3f167efd43a5cae0318a878fb9ad1ec4d43f5fc
gattlin1 <gattlin1@live.missouristate.edu>
2020-12-15 20:32:52 -0500

    Initial commit of list and conditionals assignment.

---
+++ lists-and-conditionals-assignment/src/components/Validation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Validation(props) {
+  let validationText = "";
+  if (props.length >= 5) {
+    validationText = "Text long enough";
+  } else {
+    validationText = "Text too short";
+  }
+  return <div>{validationText}</div>;
+}
+
+export default Validation;
