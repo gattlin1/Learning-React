c4f6fc3715b82a2d6b602f81ca12cf3eca185334
gattlin1 <gattlin1@live.missouristate.edu>
2021-01-14 10:18:32 -0500

    Example of cleanup work using clearTimeout to stop the alert on the timer.

--- basics/src/components/Cockpit/Cockpit.js
+++ basics/src/components/Cockpit/Cockpit.js
@@ -4,10 +4,11 @@ import classes from './Cockpit.css';
 const Cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       alert('saved data to cloud');
     }, 1000);
     return () => {
+      clearTimeout(timer);
       console.log('[Cockpit.js] cleanup work in useEffect');
     };
   }, []);
