e39ffd0ce8e8a7d3956d8023e761019ca9a3086b
gattlin1 <gattlin1@live.missouristate.edu>
2021-01-20 18:23:35 -0500

    Adding build controls to the ui.

--- burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,8 @@ const BuildControl = (props) => {
   return (
     <div className='BuildControl'>
       <div className='Label'>{props.label}</div>
-      <button className='Less'>-</button>
-      <button className='More'>+</button>
+      <button className='Less'>Less</button>
+      <button className='More'>More</button>
     </div>
   );
 };
--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import './BuildControls.css';
 
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
 const BuildControls = (props) => {
-  return <div></div>;
+  return (
+    <div className='BuildControls'>
+      {controls.map((ingredient) => {
+        return (
+          <BuildControl
+            key={ingredient.type}
+            label={ingredient.label}
+            type={ingredient.type}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default BuildControls;
