7833754dc714fd5ad4550332b1b437fdd3ab92f6
gattlin1 <gattlin1@live.missouristate.edu>
2021-01-25 14:21:19 -0500

    use axios instances to configure specific versions of axios.

---
+++ http/src/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+instance.defaults.headers.post['Content-type'] = 'application/json';
+
+instance.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    // Edit request config here
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (request) => {
+    console.log(request);
+    // Edit request config here
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
--- http/src/index.js
+++ http/src/index.js
@@ -3,35 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-type'] = 'application/json';
-
-axios.interceptors.request.use(
-  (request) => {
-    console.log(request);
-    // Edit request config here
-    return request;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
-
-axios.interceptors.response.use(
-  (request) => {
-    console.log(request);
-    // Edit request config here
-    return request;
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
