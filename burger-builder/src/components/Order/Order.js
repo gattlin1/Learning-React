8056c5e7f4317b20d577730b60c7061365b86439
gattlin1 <gattlin1@live.missouristate.edu>
2021-01-31 15:01:35 -0500

    Finishing up orders page and styling individual order components.

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -2,11 +2,37 @@ import React from 'react';
 import './Order.css';
 
 function Order(props) {
+  const ingredients = [];
+  for (const ingredient in props.ingredients) {
+    ingredients.push({
+      name: ingredient,
+      amount: props.ingredients[ingredient],
+    });
+  }
+  console.log(ingredients);
+
+  const ingredientOutput = ingredients.map((ingredient) => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px',
+        }}
+        key={ingredient.name}
+      >
+        {ingredient.name}: ({ingredient.amount})
+      </span>
+    );
+  });
+
   return (
     <div className='Order'>
-      <p>Ingredients: Salad </p>
+      <p>Ingredients: {ingredientOutput}</p>
       <p>
-        Price: <strong>5</strong>
+        Price: <strong>{props.price.toFixed(2)}</strong>
       </p>
     </div>
   );
