69a2fdde4a5baf40dcee13d226ca4e11d2ce2726
gattlin1 <gattlin1@live.missouristate.edu>
2021-02-10 22:37:38 -0500

    Starting redux assignment.

---
+++ redux--assignment-2-problem/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import Reducer from './store/reducer';
+
+const store = createStore(Reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
---
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -0,0 +1,4 @@
+const initialState = {};
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
