2355419cf7664e575024ecc87a63462bb8526c0c
gattlin1 <gattlin1@live.missouristate.edu>
2021-02-10 23:02:11 -0500

    Finished the redux assignment.

--- redux--assignment-2-problem/src/store/reducer.js
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -1,4 +1,28 @@
-const initialState = {};
-const reducer = (state = initialState, action) => {};
+import * as actionTypes from './actions';
+
+const initialState = { persons: [] };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_PERSON: {
+      return {
+        ...state,
+        persons: state.persons.concat({
+          name: action.payload.person.name,
+          id: action.payload.person.id,
+          age: action.payload.person.age,
+        }),
+      };
+    }
+    case actionTypes.DELETE_PERSON: {
+      const newPersons = state.persons.filter(
+        (person) => person.id !== action.payload.person.id
+      );
+
+      return { ...state, persons: newPersons };
+    }
+    default:
+      return state;
+  }
+};
 
 export default reducer;
