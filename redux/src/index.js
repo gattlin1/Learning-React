dfc17a15420d7a1db044811b2631e63e792d8e49
gattlin1 <gattlin1@live.missouristate.edu>
2021-02-14 09:08:21 -0500

    Applying custom middleware  to redux store.

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -12,7 +12,18 @@ const reducer = combineReducers({
   counter: counterReducer,
   result: resultReducer,
 });
-const store = createStore(reducer);
+
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    };
+  };
+};
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
