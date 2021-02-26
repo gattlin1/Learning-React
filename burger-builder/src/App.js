950633d4b33941b805a69ebd3d02f62e86b21935
gattlin1 <gattlin1@live.missouristate.edu>
2021-02-26 13:01:56 -0500

    Adding lazy loading.

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
   componentDidMount() {
@@ -18,7 +28,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path='/auth' component={Auth} />
+        <Route path='/auth' component={asyncAuth} />
         <Route path='/' component={BurgerBuilder} exact />
         <Redirect to='/' />
       </Switch>
@@ -28,9 +38,9 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path='/logout' component={Logout} />
-          <Route path='/auth' component={Auth} />
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
+          <Route path='/auth' component={asyncAuth} />
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' component={asyncOrders} />
           <Route path='/' component={BurgerBuilder} exact />
           <Redirect to='/' />
         </Switch>
