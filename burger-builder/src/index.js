7c11096d66feb660adaf2fa88462dfe91b2ec411
gattlin1 <gattlin1@live.missouristate.edu>
2021-02-26 12:43:58 -0500

    Making redux dev tools only available in dev mode.

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -12,7 +12,10 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 import { BrowserRouter } from 'react-router-dom';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 const combinedReducers = combineReducers({
   burgerBuilder: burgerReducer,
   order: orderReducer,
